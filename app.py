b4943d2a8f7bc7583907a3dc952c82670c89655b
mistrypalak432-droid <mistrypalak432@gmail.com>
2026-02-22 11:38:51 +0530

    Make app compatible with Render - use environment variables for PORT and SECRET_KEY

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
+import os
 from flask import Flask, render_template, request, redirect, url_for, abort
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'secretkey'
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'dev-key-change-in-production')
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
@@ -164,4 +165,5 @@ def result(topic):
 if __name__ == "__main__":
     with app.app_context():
         db.create_all()
-    app.run(host="0.0.0.0", port=10000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
